168fdf00b5b9064caf3344f341c75f4bcf8c2bd4
Simon Legner <Simon.Legner@gmail.com>
2022-12-11 01:46:56 +0100

    eaws-bulletin-map

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,179 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import * as L from "leaflet";
+import type { PathOptions } from "leaflet";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import "leaflet.vectorgrid/dist/Leaflet.VectorGrid.bundled.js";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import "./style.css";
+import "leaflet/dist/leaflet.css";
+
+const url = new URL(location.href);
+const date = url.searchParams.get("date") || "";
+if (!date) {
+  location.replace(
+    "/?date=" + new Date().toISOString().slice(0, "2006-01-02".length)
+  );
+}
+
+const map = initMap();
+
+const regions = [
+  "AD",
+  "AT-02",
+  "AT-03",
+  "AT-04",
+  "AT-05",
+  "AT-06",
+  "AT-07",
+  "AT-08",
+  "CH",
+  "CZ",
+  "DE-BY",
+  "ES-CT-L",
+  "ES-CT",
+  "ES",
+  "FR",
+  "GB",
+  "IS",
+  "IT-21",
+  "IT-23",
+  "IT-25",
+  "IT-32-BZ",
+  "IT-32-TN",
+  "IT-34",
+  "IT-36",
+  "IT-57",
+  "NO",
+  "PL",
+  "PL-12",
+  "SE",
+  "SI",
+  "SK",
+];
+
+Promise.all(regions.map((region) => fetchBulletins(date, region)))
+  .then((maxDangerRatings) =>
+    Object.fromEntries([...maxDangerRatings.flatMap((o) => Object.entries(o))])
+  )
+  .then((maxDangerRatings) => buildMap(maxDangerRatings, date));
+
+type Region = string;
+type WarnLevelNumber = 0 | 1 | 2 | 3 | 4 | 5;
+type MaxDangerRatings = Record<Region, WarnLevelNumber>;
+
+interface MicroRegionElevationProperties {
+  id: string;
+  elevation: "high" | "low";
+  "elevation line_visualization"?: number;
+  threshold?: number;
+  start_date?: string;
+  end_date?: string;
+}
+
+interface MicroRegionProperties {
+  id: string;
+  start_date?: string;
+  end_date?: string;
+}
+
+function initMap() {
+  const mapElement = document.querySelector<HTMLDivElement>("#map")!;
+  const map = L.map(mapElement, {
+    center: { lat: 47.3, lng: 11.3 },
+    zoom: 5,
+  });
+  L.tileLayer("https://static.avalanche.report/tms/{z}/{x}/{y}.webp", {
+    attribution: "albina-euregio",
+    maxZoom: 12,
+    minZoom: 3,
+  }).addTo(map);
+  return map;
+}
+
+async function fetchBulletins(
+  date: string,
+  region: Region = ""
+): Promise<MaxDangerRatings> {
+  const res = await fetch(
+    `https://static.avalanche.report/eaws_bulletins/${date}/${date}${
+      region ? "-" + region : ""
+    }.ratings.json`,
+    { cache: "no-cache" }
+  );
+  if (!res.ok) return {};
+  try {
+    const json = await res.json();
+    return json.maxDangerRatings;
+  } catch {
+    return {};
+  }
+}
+
+async function buildMap(
+  maxDangerRatings: MaxDangerRatings,
+  date: string,
+  ampm = ""
+) {
+  const hidden: PathOptions = Object.freeze({ stroke: false, fill: false });
+  const style = (id: string): PathOptions => {
+    if (ampm) id += ":" + ampm;
+    const warnlevel = maxDangerRatings[id];
+    if (!warnlevel) return hidden;
+    return WARNLEVEL_STYLES[warnlevel];
+  };
+  L.vectorGrid
+    .protobuf("https://static.avalanche.report/eaws_pbf/{z}/{x}/{y}.pbf", {
+      attribution: "eaws/eaws-regions, albina-euregio/pyAvaCore",
+      pane: "overlayPane",
+      interactive: false,
+      maxNativeZoom: 10,
+      vectorTileLayerStyles: {
+        "micro-regions_elevation"(
+          properties: MicroRegionElevationProperties
+        ): PathOptions {
+          if (eawsRegionsWithoutElevation.test(properties.id)) return hidden;
+          if (!filterFeature({ properties }, date)) return hidden;
+          return style(properties.id + ":" + properties.elevation);
+        },
+        "micro-regions"(properties: MicroRegionProperties): PathOptions {
+          if (!eawsRegionsWithoutElevation.test(properties.id)) return hidden;
+          if (!filterFeature({ properties }, date)) return hidden;
+          return style(properties.id);
+        },
+        outline(): PathOptions {
+          return hidden;
+        },
+      },
+    })
+    .addTo(map);
+}
+
+function filterFeature(
+  feature: GeoJSON.Feature<GeoJSON.Geometry, MicroRegionProperties>,
+  today: string = new Date().toISOString().slice(0, "2006-01-02".length)
+): boolean {
+  const properties: MicroRegionProperties = feature.properties;
+  return (
+    (!properties.start_date || properties.start_date <= today) &&
+    (!properties.end_date || properties.end_date > today)
+  );
+}
+const eawsRegionsWithoutElevation =
+  /^(AD|CH|CZ|ES-GA|ES-JA|ES-NA|ES-RI|ES-SO|ES-CT-RF|ES-CT-PA|ES-CT-PP|ES-CT-VN|ES-CT-TF|ES-CT-PR|ES-CT-L-04|FI|FR|GB|IS|NO|PL|SK)/;
+
+const WARNLEVEL_COLORS = Object.freeze([
+  "#ffffff",
+  "#ccff66",
+  "#ffff00",
+  "#ff9900",
+  "#ff0000",
+  "#000000",
+]);
+
+const WARNLEVEL_STYLES = WARNLEVEL_COLORS.map((fillColor) =>
+  Object.freeze({
+    stroke: false,
+    fill: true,
+    fillColor,
+    fillOpacity: 1.0,
+  })
+);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+import type { VectorGrid } from "leaflet";
+
+declare module "@react-leaflet/core" {
+  interface LeafletContextInterface {
+    vectorGrid: VectorGrid;
+  }
+}
