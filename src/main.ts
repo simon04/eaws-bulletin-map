bbfdf1353ca38a460088a1bdf3936dcc805d7ac0
Simon Legner <Simon.Legner@gmail.com>
2024-01-26 11:07:12 +0100

    UA

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ if (!date || !regions) {
   route(
     date || now,
     regions ||
-      "AD AT-02 AT-03 AT-04 AT-05 AT-06 AT-07 AT-08 CH CZ DE-BY ES-CT-L ES-CT ES FI FR GB IS IT-21 IT-23 IT-25 IT-32-BZ IT-32-TN IT-34 IT-36 IT-57 NO PL PL-12 SE SI SK",
+      "AD AT-02 AT-03 AT-04 AT-05 AT-06 AT-07 AT-08 CH CZ DE-BY ES-CT-L ES-CT ES FI FR GB IS IT-21 IT-23 IT-25 IT-32-BZ IT-32-TN IT-34 IT-36 IT-57 NO PL PL-12 SE SI SK UA",
     true,
   );
 }
