176f32e197689986577cdc662c19f88d4624e92a
Simon Legner <Simon.Legner@gmail.com>
2025-05-29 16:35:27 +0200

    Extract route.ts

--- src/main.ts
+++ src/main.ts
@@ -25,42 +25,12 @@ import type {
   MicroRegionElevationProperties,
   MicroRegionProperties,
   Region,
-  Regions,
 } from "./types";
 import { type AvalancheBulletin } from "./caaml";
 import { DANGER_RATINGS } from "./danger-ratings";
 import { fetchBulletins } from "./fetchBulletins";
 import { dangerRatingLink, formatBulletin } from "./formatBulletin";
-
-const searchParams = new URL(location.href).searchParams;
-const date = searchParams.get("date") || "";
-const regions: Regions = searchParams.get("regions") || "";
-const bbox = searchParams.get("bbox") || "";
-export const details = searchParams.get("details") || "";
-if (!date || !regions) {
-  const now = new Date();
-  if (now.getHours() >= 17) now.setDate(now.getDate() + 1); // tomorrow
-  route(
-    date || now,
-    regions ||
-      "AD AT-02 AT-03 AT-04 AT-05 AT-06 AT-07 AT-08 CH CZ DE-BY ES-CT-L ES-CT ES FI FR GB IS IT-21 IT-23 IT-25 IT-32-BZ IT-32-TN IT-34 IT-36 IT-57 NO PL PL-12 SE SI SK UA",
-    true,
-  );
-}
-
-function route(date: string | Date, regions: Regions, replace = false) {
-  if (date instanceof Date) {
-    date = date.toISOString().slice(0, "2006-01-02".length);
-  }
-  const params = new URLSearchParams({ date, regions, bbox, details });
-  params.forEach((value, key) => value || params.delete(key));
-  const url = "?" + params;
-  if (replace) {
-    location.replace(url);
-  } else {
-    location.assign(url);
-  }
-}
+import { date, regions, route, bbox, details } from "./route";
 
 const popup = new Popup({
   popupClass: "default",
---
+++ src/route.ts
@@ -0,0 +1,30 @@
+import type { Regions } from "./types";
+
+const searchParams = new URL(location.search).searchParams;
+export const date = searchParams.get("date") || "";
+export const regions: Regions = searchParams.get("regions") || "";
+export const bbox = searchParams.get("bbox") || "";
+export const details = searchParams.get("details") || "";
+if (!date || !regions) {
+  const now = new Date();
+  if (now.getHours() >= 17) now.setDate(now.getDate() + 1); // tomorrow
+  route(
+    date || now,
+    regions ||
+      "AD AT-02 AT-03 AT-04 AT-05 AT-06 AT-07 AT-08 CH CZ DE-BY ES-CT-L ES-CT ES FI FR GB IS IT-21 IT-23 IT-25 IT-32-BZ IT-32-TN IT-34 IT-36 IT-57 NO PL PL-12 SE SI SK UA",
+    true,
+  );
+}
+export function route(date: string | Date, regions: Regions, replace = false) {
+  if (date instanceof Date) {
+    date = date.toISOString().slice(0, "2006-01-02".length);
+  }
+  const params = new URLSearchParams({ date, regions, bbox, details });
+  params.forEach((value, key) => value || params.delete(key));
+  const url = "?" + params;
+  if (replace) {
+    location.replace(url);
+  } else {
+    location.assign(url);
+  }
+}
