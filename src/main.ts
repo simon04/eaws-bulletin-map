1733a291c903ea182e9878f39f3dd70f8f701472
Simon Legner <Simon.Legner@gmail.com>
2026-05-27 22:21:23 +0200

    Open source licenses via Vite

--- src/main.ts
+++ src/main.ts
@@ -103,7 +103,7 @@ function initMap() {
         source: new XYZ({
           url: "https://static.avalanche.report/tms/{z}/{x}/{y}.webp",
           attributions: [
-            '🌍 <a href="https://github.com/simon04/eaws-bulletin-map">simon04/eaws-bulletin-map</a> (GPLv3)',
+            '🌍 <a href="https://github.com/simon04/eaws-bulletin-map">simon04/eaws-bulletin-map</a> (GPLv3, <a href="/assets/license.txt">open source licenses</a>)',
             '<a href="https://gitlab.com/albina-euregio">albina-euregio</a> (GPLv3, CC BY 4.0)',
             '<a href="https://sonny.4lima.de/">Sonny</a> (CC BY 4.0)',
             '<a href="https://www.eea.europa.eu/en/datahub/datahubitem-view/d08852bc-7b5f-4835-a776-08362e2fbf4b">EU-DEM</a> (CC BY 4.0)',
